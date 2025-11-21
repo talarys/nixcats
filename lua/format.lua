74f1e0d202f7771603cd56e572d5d9768493744a
talarys <d.simon.ssx0@gmail.com>
2025-11-21 18:57:30 +0100

    Refactor formatters in format.lua to remove unused lua formatter

--- lua/format.lua
+++ lua/format.lua
@@ -1,11 +1,10 @@
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff" },
-        bash = { "shfmt" },
-    },
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-    },
+  formatters_by_ft = {
+    python = { "ruff" },
+    bash = { "shfmt" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
