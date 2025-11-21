d556779e3a6cb7c76e039d87d1186d95df86d634
talarys <d.simon.ssx0@gmail.com>
2025-11-21 20:13:27 +0100

    Theme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -7,17 +7,38 @@ local function copy_highlight(source_group, target_group, attribute)
     end
 end
 
+local function get_hlgroup(group)
+    local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = group })
+    if ok then
+        return hl
+    else
+        return nil
+    end
+end
+
+local function set_hlgroup(group, attrs)
+    vim.api.nvim_set_hl(0, group, attrs)
+end
+
 vim.api.nvim_create_autocmd('ColorScheme', {
     pattern = '*',
     callback = function()
-        copy_highlight('Normal', 'NormalFloat', 'bg')
-        copy_highlight('Normal', 'NormalFloat', 'fg')
+        local bg = get_hlgroup('Normal').bg
+        local bg_alt = get_hlgroup('Visual').bg
+        local green = get_hlgroup('String').fg
+        local red = get_hlgroup('Error').fg
 
-        copy_highlight('Normal', 'FloatBorder', 'bg')
-        copy_highlight('Normal', 'FloatBorder', 'fg')
-
-        -- copy_highlight('Normal', 'Pmenu', 'bg')
-        -- copy_highlight('CursorLine', 'PmenuSel', 'bg')
+        set_hlgroup("SnacksPickerBorder", { fg = bg_alt, bg = bg })
+        set_hlgroup("SnacksPicker", { bg = bg })
+        set_hlgroup("SnacksPickerPreviewBorder", { fg = bg, bg = bg })
+        set_hlgroup("SnacksPickerPreview", { bg = bg })
+        set_hlgroup("SnacksPickerPreviewTitle", { fg = bg, bg = green })
+        set_hlgroup("SnacksPickerBoxBorder", { fg = bg, bg = bg })
+        set_hlgroup("SnacksPickerInputBorder", { fg = bg, bg = bg })
+        set_hlgroup("SnacksPickerInputSearch", { fg = red, bg = bg })
+        set_hlgroup("SnacksPickerListBorder", { fg = bg, bg = bg })
+        set_hlgroup("SnacksPickerList", { bg = bg })
+        set_hlgroup("SnacksPickerListTitle", { fg = bg, bg = bg })
     end,
 })
 
