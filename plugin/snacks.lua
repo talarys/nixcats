b7846932ee176b676f77640db7bad52d41237867
talarys <d.simon.ssx0@gmail.com>
2025-11-21 19:32:04 +0100

    Fix

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -1,25 +1,25 @@
 require("snacks").setup({
-	animate = { enabled = true },
-	bigfile = { enabled = true },
-	explorer = { enabled = true },
-	gh = { enabled = true },
-	gitbrowse = { enabled = true },
-	image = { enabled = true },
-	indent = { enabled = true },
-	input = { enabled = true },
-	lazygit = { enabled = true },
-	notifier = { enabled = true, timeout = 3000, },
-	picker = { enabled = true },
-	profiler = { enabled = true },
-	quickfile = { enabled = true },
-	scope = { enabled = true },
-	scratch = { enabled = true },
-	scroll = { enabled = true },
-	statuscolumn = { enabled = true },
-	terminal = { enabled = true },
-	toggle = { enabled = true },
-	util = { enabled = true },
-	words = { enabled = true },
+  animate = { enabled = true },
+  bigfile = { enabled = true },
+  explorer = { enabled = true },
+  gh = { enabled = true },
+  gitbrowse = { enabled = true },
+  image = { enabled = true },
+  indent = { enabled = true },
+  input = { enabled = true },
+  lazygit = { enabled = true },
+  notifier = { enabled = true, timeout = 3000, },
+  picker = { enabled = true },
+  profiler = { enabled = true },
+  quickfile = { enabled = true },
+  scope = { enabled = true },
+  scratch = { enabled = true },
+  scroll = { enabled = true },
+  statuscolumn = { enabled = true },
+  terminal = { enabled = true },
+  toggle = { enabled = true },
+  util = { enabled = true },
+  words = { enabled = true },
 })
 
 -- Top Pickers & Explorer
@@ -33,7 +33,7 @@ vim.keymap.set("n", "<leader>e", function() Snacks.explorer() end, { desc = "Fil
 -- find
 vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,
-	{ desc = "Find Config Fie" })
+  { desc = "Find Config Fie" })
 vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", function() Snacks.picker.git_files() end, { desc = "Find Git Files" })
 vim.keymap.set("n", "<leader>fp", function() Snacks.picker.projects() end, { desc = "Projects" })
@@ -51,10 +51,10 @@ vim.keymap.set("n", "<leader>gf", function() Snacks.picker.git_log_file() end, {
 -- gh
 vim.keymap.set("n", "<leader>gi", function() Snacks.picker.gh_issue() end, { desc = "GitHub Issues (open)" })
 vim.keymap.set("n", "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end,
-	{ desc = "GitHub Issues (all)" })
+  { desc = "GitHub Issues (all)" })
 vim.keymap.set("n", "<leader>gp", function() Snacks.picker.gh_pr() end, { desc = "GitHub Pull Requests (open)" })
 vim.keymap.set("n", "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end,
-	{ desc = "GitHub Pull Requests (all)" })
+  { desc = "GitHub Pull Requests (all)" })
 
 -- Grep
 vim.keymap.set("n", "<leader>sb", function() Snacks.picker.lines() end, { desc = "Buffer Lines" })
@@ -95,7 +95,7 @@ vim.keymap.set("n", "gai", function() Snacks.picker.lsp_incoming_calls() end, { 
 vim.keymap.set("n", "gao", function() Snacks.picker.lsp_outgoing_calls() end, { desc = "C[a]lls Outgoing" })
 vim.keymap.set("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, { desc = "LSP Symbols" })
 vim.keymap.set("n", "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end,
-	{ desc = "LSP Workspace Symbols" })
+  { desc = "LSP Workspace Symbols" })
 
 -- Other
 vim.keymap.set("n", "<leader>z", function() Snacks.zen() end, { desc = "Toggle Zen Mode" })
@@ -113,38 +113,15 @@ vim.keymap.set("n", "<c-_>", function() Snacks.terminal() end, { desc = "which_k
 vim.keymap.set("n", "]]", function() Snacks.words.jump(vim.v.count1) end, { desc = "Next Reference" })
 vim.keymap.set("n", "[[", function() Snacks.words.jump(-vim.v.count1) end, { desc = "Prev Reference" })
 
-vim.api.nvim_create_autocmd("User", {
-	pattern = "VeryLazy",
-	callback = function()
-		-- Setup some globals for debugging (lazy-loaded)
-		_G.dd = function(...)
-			Snacks.debug.inspect(...)
-		end
-		_G.bt = function()
-			Snacks.debug.backtrace()
-		end
-
-		-- Override print to use snacks for `:=` command
-		if vim.fn.has("nvim-0.11") == 1 then
-			vim._print = function(_, ...)
-				dd(...)
-			end
-		else
-			vim.print = _G.dd
-		end
-
-		-- Create some toggle mappings
-		Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-		Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-		Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-		Snacks.toggle.diagnostics():map("<leader>ud")
-		Snacks.toggle.line_number():map("<leader>ul")
-		Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
-			"<leader>uc")
-		Snacks.toggle.treesitter():map("<leader>uT")
-		Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-		Snacks.toggle.inlay_hints():map("<leader>uh")
-		Snacks.toggle.indent():map("<leader>ug")
-		Snacks.toggle.dim():map("<leader>uD")
-	end,
-})
+Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+Snacks.toggle.diagnostics():map("<leader>ud")
+Snacks.toggle.line_number():map("<leader>ul")
+Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
+  "<leader>uc")
+Snacks.toggle.treesitter():map("<leader>uT")
+Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+Snacks.toggle.inlay_hints():map("<leader>uh")
+Snacks.toggle.indent():map("<leader>ug")
+Snacks.toggle.dim():map("<leader>uD")
