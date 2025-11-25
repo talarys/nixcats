3bccdb0a799c96c3ea03b1c38a7022e0755f2f7e
talarys <d.simon.ssx0@gmail.com>
2025-11-25 14:13:32 +0100

    Added trouble to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,21 @@
+local trouble = require("trouble")
+local symbols = trouble.statusline {
+  mode = "lsp_document_symbols",
+  groups = {},
+  title = false,
+  filter = { range = true },
+  format = "{kind_icon}{symbol.name:Normal}",
+  -- The following line is needed to fix the background color
+  -- Set it to the lualine section you want to use
+  hl_group = "lualine_c_normal",
+}
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -36,8 +48,8 @@ require('lualine').setup {
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_y = { 'location' },
+    lualine_z = { 'lsp_status' }
   },
   inactive_sections = {
     lualine_a = {},
@@ -49,7 +61,7 @@ require('lualine').setup {
   },
   tabline = {},
   winbar = {
-    lualine_a = {},
+    lualine_a = { symbols.get },
     lualine_b = {},
     lualine_c = {},
     lualine_x = {},
