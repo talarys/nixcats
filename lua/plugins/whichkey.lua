e0db640809b6e9c77a1ad1ce58676e893ff8a2ae
talarys <d.simon.ssx0@gmail.com>
2025-11-25 21:51:17 +0100

    Configured whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,4 +1,17 @@
 require("which-key").setup {
   preset = "helix",
-  icons = { mappings = false }
+  icons = { mappings = false },
+  spec = {
+    mode = { "n", "x" },
+    { "<leader><tab>", group = "tabs" },
+    { "<leader>b",     group = "buffers" },
+    { "<leader>f",     group = "find" },
+    { "<leader>g",     group = "git" },
+    { "<leader>l",     group = "lsp" },
+    { "<leader>q",     group = "quit" },
+    { "<leader>s",     group = "search" },
+    { "<leader>u",     group = "toggles" },
+    { "<leader>w",     group = "windows" },
+    { "<leader>.",     group = "scratch" },
+  }
 }
