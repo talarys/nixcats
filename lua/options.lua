1fd45c962620b1fee73f59bcaf50d4d3931b65c4
talarys <d.simon.ssx0@gmail.com>
2025-11-25 18:05:16 +0100

    Changed border

--- lua/options.lua
+++ lua/options.lua
@@ -23,7 +23,7 @@ vim.o.signcolumn     = 'yes'               -- Always show signcolumn (less flick
 vim.o.splitbelow     = true                -- Horizontal splits will be below
 vim.o.splitkeep      = 'screen'            -- Reduce scroll during window split
 vim.o.splitright     = true                -- Vertical splits will be to the right
-vim.o.winborder      = 'single'            -- Use border in floating windows
+vim.o.winborder      = 'rounded'           -- Use border in floating windows
 vim.o.wrap           = false               -- Don't visually wrap lines (toggle with \w)
 
 vim.o.cursorlineopt  = 'screenline,number' -- Show cursor line per screen line
